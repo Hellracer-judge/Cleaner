c0b59524367723217bcf3257dc59d5429a9286d0
Accelerator74 <accelerator74@gmail.com>
2016-12-25 15:18:15 +0500

    Update extension.cpp

--- extension.cpp
+++ extension.cpp
@@ -31,6 +31,46 @@ DETOUR_DECL_STATIC2(Detour_DefSpew, SpewRetval_t, SpewType_t, channel, char *, t
 }
 #endif
 
+#ifdef WIN32
+//https://github.com/alliedmodders/sourcemod/blob/237db0504c7a59e394828446af3e8ca3d53ef647/extensions/sdktools/vglobals.cpp#L149
+size_t UTIL_StringToSignature(const char *str, char buffer[], size_t maxlength)
+{
+	size_t real_bytes = 0;
+	size_t length = strlen(str);
+
+	for (size_t i=0; i<length; i++)
+	{
+		if (real_bytes >= maxlength)
+		{
+			break;
+		}
+		buffer[real_bytes++] = (unsigned char)str[i];
+		if (str[i] == '\\'
+			&& str[i+1] == 'x')
+		{
+			if (i + 3 >= length)
+			{
+				continue;
+			}
+			/* Get the hex part */
+			char s_byte[3];
+			int r_byte;
+			s_byte[0] = str[i+2];
+			s_byte[1] = str[i+3];
+			s_byte[2] = '\n';
+			/* Read it as an integer */
+			sscanf(s_byte, "%x", &r_byte);
+			/* Save the value */
+			buffer[real_bytes-1] = (unsigned char)r_byte;
+			/* Adjust index */
+			i += 3;
+		}
+	}
+
+	return real_bytes;
+}
+#endif
+
 bool Cleaner::SDK_OnLoad(char *error, size_t maxlength, bool late)
 {
 	SM_GET_IFACE(MEMORYUTILS, memutils);
@@ -85,7 +125,7 @@ bool Cleaner::SDK_OnLoad(char *error, size_t maxlength, bool late)
 #ifdef PLATFORM_WINDOWS
 	HMODULE tier0 = GetModuleHandle("tier0.dll");
 	char sig[256];
-	size_t size = UTIL_StringToSignature(g_pGameConf->GetKeyValue("ServerConsolePrintSig_windows"), sig, sizeof(sig)));
+	size_t size = UTIL_StringToSignature(g_pGameConf->GetKeyValue("ServerConsolePrintSig_windows"), sig, sizeof(sig));
 	void * fn = memutils->FindPattern(tier0, sig, size);
 #elif defined PLATFORM_LINUX
 	void * tier0 = dlopen("libtier0.so", RTLD_NOW);
